e237eb736f5529ab44f1897e1303aca141a77a29
Ben <ben@alwaysblank.dev>
2021-10-31 11:05:39 -0700

    Set up child/parent behavior

--- src/filters.php
+++ src/filters.php
@@ -1,11 +1,21 @@
 <?php
+/**
+ * When composer builds its autoloader, `add_filter` doesn't exist but Composer
+ * will try to run it, so let's avoid that error.
+ */
+if ( function_exists( 'add_filter' ) ) {
 
-add_filter( 'template-dir/default-args', function ( $args ) {
-	if ( class_exists( '\\Roots\\Acorn\\View\\ViewServiceProvider' ) ) {
-		// This is probably a theme using Acorn, so change the default roots.
-		$args['template_root'] = get_theme_file_path( '/resources/views' );
-		$args['parent_template_root'] = get_parent_theme_file_path( '/resources/views' );
-	}
+	/**
+	 * Acorn (often used in Sage themes) has a different path to views that we
+	 * can predict, so lets set that up right now.
+	 */
+	add_filter( 'template-dir/default-args', function ( $args ) {
+		if ( class_exists( '\\Roots\\Acorn\\View\\ViewServiceProvider' ) ) {
+			// This is probably a theme using Acorn, so change the default roots.
+			$args['template_root'] = get_theme_file_path( '/resources/views' );
+			$args['parent_template_root'] = get_parent_theme_file_path( '/resources/views' );
+		}
 
-	return $args;
-}, 9 );
+		return $args;
+	}, 9 );
+}
