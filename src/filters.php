6b0e041a434a94a6a3ac03c39c586f0c8b7566a0
Ben <ben@alwaysblank.dev>
2021-10-31 12:06:18 -0700

    Extract acorn filter to function

--- src/filters.php
+++ src/filters.php
@@ -1,4 +1,7 @@
 <?php
+
+namespace Livy\Plumbing\Templates;
+
 /**
  * When composer builds its autoloader, `add_filter` doesn't exist but Composer
  * will try to run it, so let's avoid that error.
@@ -7,15 +10,25 @@ if ( function_exists( 'add_filter' ) ) {
 
 	/**
 	 * Acorn (often used in Sage themes) has a different path to views that we
-	 * can predict, so lets set that up right now.
+	 * can predict, so lets set that up right now. It's hooked up a bit early
+	 * to make it easier to unhook if necessary.
 	 */
-	add_filter( 'template-dir/default-args', function ( $args ) {
-		if ( class_exists( '\\Roots\\Acorn\\View\\ViewServiceProvider' ) ) {
-			// This is probably a theme using Acorn, so change the default roots.
-			$args['template_root'] = get_theme_file_path( '/resources/views' );
-			$args['parent_template_root'] = get_parent_theme_file_path( '/resources/views' );
-		}
+	add_filter( 'template-dir/default-args', __NAMESPACE__ . '\\set_acorn_defaults', 9 );
+}
 
-		return $args;
-	}, 9 );
+/**
+ * Adjust defaults for Acorn, if Acorn is detected.
+ *
+ * @param array $args
+ *
+ * @return array
+ */
+function set_acorn_defaults( array $args ) : array {
+	if ( class_exists( '\\Roots\\Acorn\\View\\ViewServiceProvider' ) ) {
+		// This is probably a theme using Acorn, so change the default roots.
+		$args['template_root'] = get_theme_file_path( '/resources/views' );
+		$args['parent_template_root'] = get_parent_theme_file_path( '/resources/views' );
+	}
+
+	return $args;
 }
